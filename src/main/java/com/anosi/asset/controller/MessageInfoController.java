3b2e886b84f3bb8b679219e14e0b6207ae0f73b2
jinyao-PC\jinyao <573380618@qq.com>
2018-01-15 18:27:44 +0800

    修复维修和转派详情页面

--- src/main/java/com/anosi/asset/controller/MessageInfoController.java
+++ src/main/java/com/anosi/asset/controller/MessageInfoController.java
@@ -1,5 +1,7 @@
 package com.anosi.asset.controller;
 
+import com.alibaba.fastjson.JSON;
+import com.google.common.collect.ImmutableMap;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -20,65 +22,73 @@ import com.anosi.asset.service.MessageInfoService;
 import com.anosi.asset.util.StringUtil;
 import com.querydsl.core.types.Predicate;
 
+import java.util.Date;
+
 @RestController
 public class MessageInfoController extends BaseController<MessageInfo> {
 
-	private static final Logger logger = LoggerFactory.getLogger(MessageInfoController.class);
+    private static final Logger logger = LoggerFactory.getLogger(MessageInfoController.class);
 
-	@Autowired
-	private MessageInfoService messageInfoService;
+    @Autowired
+    private MessageInfoService messageInfoService;
 
-	/***
-	 * 查看某条信息
-	 * 
-	 * @param id
-	 * @return
-	 */
-	@RequestMapping(value = "/messageInfo/{id}/view", method = RequestMethod.GET)
-	public ModelAndView toViewFileMetaData(@PathVariable Long id) {
-		logger.debug("view messageInfo");
-		return new ModelAndView("messageInfo/viewMessageInfo", "id", id);
-	}
+    /***
+     * 查看某条信息
+     *
+     * @param id
+     * @return
+     */
+    @RequestMapping(value = "/messageInfo/{id}/view", method = RequestMethod.GET)
+    public ModelAndView toViewFileMetaData(@PathVariable Long id) {
+        logger.debug("view messageInfo");
+        MessageInfo messageInfo = messageInfoService.getOne(id);
+        // 设置阅读时间
+        if (messageInfo.getReadTime() == null) {
+            messageInfo.setReadTime(new Date());
+            messageInfoService.save(messageInfo);
+        }
+        return new ModelAndView("messageInfo/viewMessageInfo", "id", id);
+    }
 
-	/***
-	 * 查询某条具体的messageInfo
-	 * 
-	 * @param showType
-	 * @param predicate
-	 * @param showAttributes
-	 * @return
-	 * @throws Exception
-	 */
-	@RequestMapping(value = "/messageInfo/management/data/one", method = RequestMethod.GET)
-	public JSONObject findMessageInfoManageDataOne(@QuerydslPredicate(root = MessageInfo.class) Predicate predicate,
-			@RequestParam(value = "showAttributes", required = false) String showAttributes) throws Exception {
-		logger.info("find messageInfo one");
-		return jsonUtil.parseAttributesToJson(StringUtil.splitAttributes(showAttributes),
-				messageInfoService.findOne(predicate));
-	}
+    /***
+     * 查询某条具体的messageInfo
+     *
+     * @param showType
+     * @param predicate
+     * @param showAttributes
+     * @return
+     * @throws Exception
+     */
+    @RequestMapping(value = "/messageInfo/management/data/one", method = RequestMethod.GET)
+    public JSONObject findMessageInfoManageDataOne(@QuerydslPredicate(root = MessageInfo.class) Predicate predicate,
+                                                   @RequestParam(value = "showAttributes", required = false) String showAttributes) throws Exception {
+        logger.info("find messageInfo one");
+        return jsonUtil.parseAttributesToJson(StringUtil.splitAttributes(showAttributes),
+                messageInfoService.findOne(predicate));
+    }
 
-	/***
-	 * 获取messageInfo数据
-	 * 
-	 * @param pageable
-	 * @param predicate
-	 * @param showAttributes
-	 * @param rowId
-	 * @param companyCode
-	 * @return
-	 * @throws Exception
-	 */
-	@RequestMapping(value = "/messageInfo/management/data/{showType}", method = RequestMethod.GET)
-	public JSONObject findMessageInfoManageData(@PathVariable ShowType showType,
-			@PageableDefault(sort = { "id" }, direction = Sort.Direction.DESC, page = 0, size = 5) Pageable pageable,
-			@QuerydslPredicate(root = MessageInfo.class) Predicate predicate,
-			@RequestParam(value = "showAttributes", required = false) String showAttributes,
-			@RequestParam(value = "rowId", required = false, defaultValue = "id") String rowId) throws Exception {
-		logger.info("find messageInfo");
-		logger.debug("page:{},size{},sort{}", pageable.getPageNumber(), pageable.getPageSize(), pageable.getSort());
-		logger.debug("rowId:{},showAttributes:{}", rowId, showAttributes);
+    /***
+     * 获取messageInfo数据
+     *
+     * @param pageable
+     * @param predicate
+     * @param showAttributes
+     * @param rowId
+     * @param companyCode
+     * @return
+     * @throws Exception
+     */
+    @RequestMapping(value = "/messageInfo/management/data/{showType}", method = RequestMethod.GET)
+    public JSONObject findMessageInfoManageData(@PathVariable ShowType showType,
+                                                @PageableDefault(sort = {"id"}, direction = Sort.Direction.DESC, page = 0, size = 20) Pageable pageable,
+                                                @QuerydslPredicate(root = MessageInfo.class) Predicate predicate,
+                                                @RequestParam(value = "showAttributes", required = false) String showAttributes,
+                                                @RequestParam(value = "rowId", required = false, defaultValue = "id") String rowId) throws Exception {
+        logger.info("find messageInfo");
+        logger.debug("page:{},size{},sort{}", pageable.getPageNumber(), pageable.getPageSize(), pageable.getSort());
+        logger.debug("rowId:{},showAttributes:{}", rowId, showAttributes);
 
-		return parseToJson(messageInfoService.findAll(predicate, pageable), rowId, showAttributes, showType);
-	}
+        return parseToJson(messageInfoService.findAll(predicate, pageable), rowId, showAttributes, showType);
+    }
 
 }
